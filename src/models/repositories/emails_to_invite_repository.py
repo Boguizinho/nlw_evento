8273a6481e630928bb6cb3002ada897c152b40aa
Gianlucca <gianboghi@hotmail.com>
2024-07-10 11:24:35 -0300

    feat: Implementing links_repository with test

--- src/models/repositories/emails_to_invite_repository.py
+++ src/models/repositories/emails_to_invite_repository.py
@@ -26,6 +26,6 @@ class EmailsToInviteRepository:
         cursor.execute(
             '''SELECT * FROM emails_to_invite Where trip_id = ?''', (trip_id,)
         )
-        trip = cursor.fetchall()
-        return trip
+        emails = cursor.fetchall()
+        return emails
     
